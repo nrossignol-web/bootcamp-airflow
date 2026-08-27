3654079d499f67cfc36d8f7a6fe640707d3db7d2
nrossignol-web <nrossignol@itsgroup.com>
2026-08-27 14:19:42 +0200

    Remove schedule_interval from hello_airflow DAG
    Removed schedule_interval from DAG definition.

--- hello_airflow.py
+++ hello_airflow.py
@@ -6,7 +6,6 @@ from airflow.operators.bash import BashOperator
 with DAG(
     dag_id="hello_airflow",
     start_date=datetime(2026, 1, 1),
-    schedule_interval="@daily",
 catchup=False,
 tags=['hello_airflow']
 ):
