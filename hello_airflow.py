e2a05971d71c04a5b947130119a0b77cfb8e50bd
nrossignol-web <nrossignol@itsgroup.com>
2026-08-27 11:56:15 +0000

    fixed

--- hello_airflow.py
+++ hello_airflow.py
@@ -2,16 +2,20 @@ from airflow import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 
-def print_hello(ti=None):
+
+def _print_hello(ti=None):
     print("hello Airflow")
 
 with DAG(
     dag_id="hello_airflow",
-    start_date=datetime(2023, 1, 1),
+    start_date=datetime(2026, 1, 1),
     schedule_interval="@daily"
+,
+catchup=False,
+tags=['hello_airflow']
 )
 :
-    task1 = PythonOperator(
+    print_hello = PythonOperator(
         task_id="print_hello",
         pyhone_callable=print_hello
     )
